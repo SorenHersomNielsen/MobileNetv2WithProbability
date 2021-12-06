92905ee3a72442bd030dbb4a6df2d71d65731a3b
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-12-06 12:07:23 +0100

    Initial Commit

---
+++ MobileNetv2Demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileNetv2Demo2
+//
+//  Created by Søren Nielsen on 06/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
